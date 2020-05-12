66ba49c780da3568a0236b5a8d5d985fa6ddcc4b
MakerbakerYT <65129039+MakerbakerYT@users.noreply.github.com>
2020-05-11 19:30:45 -0600

    Update Task.php

--- src/core/entity/Task.php
+++ src/core/entity/Task.php
@@ -31,7 +31,7 @@ use pocketmine\network\mcpe\protocol\PlayerActionPacket;
 use pocketmine\scheduler\Task;
 use pocketmine\utils\TextFormat;
 
-class($player, $block) extends Task {
+class EntityListener implements Listener {
 
     /** @var Cryptic */
     private $core;
